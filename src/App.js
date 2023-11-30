2d923915c1ecb1fe84cc1db7f4403543f5b2f2ff
Steve Ives <steve.ives@synergex.com>
2023-11-29 16:24:09 -0800

    Updated with what we learned about getting logout working.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 // src/App.js
+
 import React, { useEffect } from 'react';
 import userManager from './authService';
 
 function App() {
   useEffect(() => {
-    // Check if we're in the callback
-    console.log(window.location.pathname)
-    if (window.location.pathname === '/loginresponse') {
-        userManager.signinRedirectCallback().then(user => {
+
+  //   userManager.getUser().then(user => {
+  //     if (user) {
+  //         console.log('User:', user);
+  //         console.log('ID Token:', user.id_token);
+  //         console.log('Access Token:', user.access_token);
+  //         console.log('User profile:', user.profile);
+  //     } else {
+  //         console.log('User not logged in');
+  //     }
+  // }).catch(error => {
+  //     console.error('Error getting user:', error);
+  // });
+
+    // // Check if we're in the callback
+    // console.log(window.location.pathname)
+
+    if (window.location.pathname === '/login') {
+      userManager.signinRedirectCallback().then(user => {
         window.location.pathname = '/';
       }).catch(err => {
         console.log(err);
       });
     }
+
+    if (window.location.pathname === '/logout') {
+      console.log('We hit /logout');
+      userManager.signoutRedirectCallback().then(user => {
+        console.log('redirecting to /');
+        window.location.pathname = '/';
+      }).catch(err => {
+        console.log(err);
+      });
+    }
+
   }, []);
 
   const login = () => {
